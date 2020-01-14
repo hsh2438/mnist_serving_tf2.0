65ebd04ffc0f365b5658f5b88e9337b12d9d908e
seokhyun <hsh2438@naver.com>
2020-01-14 14:28:46 +0900

    Update start_serving.sh

--- start_serving.sh
+++ start_serving.sh
@@ -1,1 +1,1 @@
-docker run -p 8501:8501 --mount type=bind,source=$PWD/mnist,target=/models/mnist -e MODEL_NAME=mnist -t tensorflow/serving:2.1.0-rc1
+docker run -p 8501:8501 --mount type=bind,source=$PWD/mnist,target=/models/mnist -e MODEL_NAME=mnist -td tensorflow/serving:2.1.0-rc1
