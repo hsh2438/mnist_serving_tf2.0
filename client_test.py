4abe61c236c288d37435624087515aeb4d32f41f
seokhyun <hsh2438@naver.com>
2020-01-14 13:16:56 +0900

    Create client_test.py

---
+++ client_test.py
@@ -0,0 +1,16 @@
+import json
+import requests
+from PIL import Image
+import numpy as np
+
+test_image = Image.open('test_image.jpg')
+pixels = np.array(test_image)
+
+address = 'http://localhost:8501/v1/models/mnist:predict'
+data = json.dumps({'instances':pixels.tolist()})
+
+result = requests.post(address, data=data)
+predictions = json.loads(str(result.content, 'utf-8'))['predictions']
+
+for prediction in predictions:
+  print(np.argmax(prediction))
